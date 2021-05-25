27afbf29d81bf16a74ac55581fa64ee48c1d5f91
Daniel <endqwerty@gmail.com>
2021-05-24 20:36:45 -0700

    add auth

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,24 @@
 import { ChatClient } from "twitch-chat-client";
 import {
-  AccessToken,
   StaticAuthProvider,
   RefreshableAuthProvider,
-  ClientCredentialsAuthProvider,
   AuthProvider,
 } from "twitch-auth";
 import dotenv from "dotenv";
 import { promises as fs } from "fs";
+import express from "express";
+import open from "open";
+import fetch from "node-fetch";
 
 dotenv.config();
 
 const clientSecret = `${process.env.clientSecret}`;
 const clientId = `${process.env.clientId}`;
+const channel = `${process.env.channel}`;
+const redirectURI = `${process.env.redirectURI}`;
 
-async function getAccessToken(
-  clientId: string,
-  clientSecret: string
-): Promise<AccessToken> {
-  const appTokenProvider = new ClientCredentialsAuthProvider(
-    clientId,
-    clientSecret
-  );
-  return appTokenProvider.getAccessToken();
-}
+const app = express();
+const state = Date.now();
 
 async function getAuthProvider(
   clientId: string,
@@ -48,11 +43,12 @@ async function getAuthProvider(
           refreshToken,
           expiryTimestamp: expiryDate === null ? null : expiryDate.getTime(),
         };
-        await fs.writeFile(
-          "./tokens.json",
-          JSON.stringify(newTokenData, null, 4),
-          "utf8"
-        );
+        // store in db for this user
+        // await fs.writeFile(
+        //   "./tokens.json",
+        //   JSON.stringify(newTokenData, null, 4),
+        //   "utf8"
+        // );
       },
     }
   );
@@ -64,21 +60,39 @@ async function createClient(chatClient: ChatClient) {
   // listen to more events...
   await chatClient.connect();
   chatClient.onMessage((channel, user, message) => {
-    if (message === "!ping") {
-      chatClient.say(channel, "Pong!");
-    } else if (message === "!dice") {
-      const diceRoll = Math.floor(Math.random() * 6) + 1;
-      chatClient.say(channel, `@${user} rolled a ${diceRoll}`);
+    // check if user has already voted
+    if (message.startsWith("!pick")) {
+      const pick = message.slice(5).trim();
+      chatClient.say(channel, pick);
     }
   });
 }
+async function main(code: string) {
+  const response = await fetch(
+    `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}&grant_type=authorization_code&redirect_uri=${redirectURI}`,
+    { method: "POST" }
+  );
+  const tokenData = await response.json();
 
-async function main() {
-  const tokenData = JSON.parse(await fs.readFile("./tokens.json", "utf8"));
-
+  // const tokenData = JSON.parse(await fs.readFile("./tokens.json", "utf8"));
   const authProvider = await getAuthProvider(clientId, tokenData);
-  const chatClient = new ChatClient(authProvider, { channels: ["endqwerty"] });
+  const chatClient = new ChatClient(authProvider, { channels: [channel] });
   createClient(chatClient);
 }
-
-main();
+app.get("/", function (req: express.Request, res: express.Response) {
+  console.log(req.query.code);
+  if (parseInt(`${req.query.state}`) == state) {
+    res.send("e7_bot has received authentication");
+    main(`${req.query.code}`);
+  } else {
+    res.send(
+      "State does not match! Potential security vulnerability detected."
+    );
+  }
+});
+app.listen(3000);
+open(
+  `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURI(
+    redirectURI
+  )}&response_type=code&scope=chat:read+chat:edit&force_verify=true&state=${state}`
+);
