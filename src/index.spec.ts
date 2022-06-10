49194ff118fee8b34ecddfb232e7c9fb17e1201d
Daniel Yang <endqwerty@gmail.com>
2022-06-10 03:14:52 -0700

    add missing files

---
+++ src/index.spec.ts
@@ -0,0 +1,14 @@
+const can = {
+  name: 'pamplemousse',
+  ounces: 12,
+};
+
+describe('the can', () => {
+  test('has 12 ounces', () => {
+    expect(can.ounces).toBe(12);
+  });
+
+  test('has a sophisticated name', () => {
+    expect(can.name).toBe('pamplemousse');
+  });
+});
